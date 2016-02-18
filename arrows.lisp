e6c651ccb51e3fe4fb54d5c58ccdb46cef4b74d1
Svante v. Erichsen <svante.v.erichsen@web.de>
2016-02-18 23:50:58 +0100

    implement diamond wand/spear, simplify/dry expansion

--- arrows.lisp
+++ arrows.lisp
@@ -1,26 +1,45 @@
 (in-package :cl-arrows)
 
+(defun simple-inserter (insert-fun)
+  (lambda (acc next)
+    (if (listp next)
+        (funcall insert-fun acc next)
+        (list next acc))))
+
 (defmacro -> (initial-form &rest forms)
-  (let ((output-form initial-form)
-        (remaining-forms forms))
-    (loop while remaining-forms do
-         (let ((current-form (car remaining-forms)))
-           (if (listp current-form)
-	       (setf output-form (cons (car current-form)
-				       (cons output-form (cdr current-form))))
-	       (setf output-form (list current-form output-form))))
-         (setf remaining-forms (cdr remaining-forms)))
-    output-form))
+  (reduce (simple-inserter #'insert-first)
+          forms
+          :initial-value initial-form))
 
 (defmacro ->> (initial-form &rest forms)
-  (let ((output-form initial-form)
-        (remaining-forms forms))
-    (loop while remaining-forms do
-         (let ((current-form (car remaining-forms)))
-	   (if (listp current-form)
-	       (setf output-form (cons (car current-form)
-				       (append (cdr current-form) (list output-form))))
-	       (setf output-form (list current-form output-form))))
-         (setf remaining-forms (cdr remaining-forms)))
-    output-form))
+  (reduce (simple-inserter #'insert-last)
+          forms
+          :initial-value initial-form))
 
+(defun diamond-inserter (insert-fun)
+  (simple-inserter (lambda (acc next)
+                     (if (find-if #'<>p next)
+                         (substitute-if acc #'<>p next)
+                         (funcall insert-fun acc next)))))
+
+(defmacro -<> (initial-form &rest forms)
+  (reduce (diamond-inserter #'insert-first)
+          forms
+          :initial-value initial-form))
+
+(defmacro -<>> (initial-form &rest forms)
+  (reduce (diamond-inserter #'insert-last)
+          forms
+          :initial-value initial-form))
+
+(defun <>p (form)
+  (and (symbolp form)
+       (string= form "<>")))
+
+(defun insert-first (arg surround)
+  (cons (car surround)
+        (append arg (cdr surround))))
+
+(defun insert-last (arg surround)
+  (cons (car surround)
+        (append (cdr surround) arg)))
